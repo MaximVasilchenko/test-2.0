dc010c784d680d30de255f619bfe815eda804e5c
MaximVasilchenko <97507673+MaximVasilchenko@users.noreply.github.com>
2022-01-23 10:56:00 +0300

    Add files via upload

---
+++ 3/script.js
@@ -0,0 +1,10 @@
+//var firstNumber = prompt ("введите 1 число")
+// var secondNuvber = prompt ("введите 2 число")
+
+// console.log(typeof(firstNumber))
+
+// alert (+firstNumber + Number(secondNuvber)) 
+
+var id = document.querySelector("js")
+
+console.log(id) 
